c829e00502f7cc615fc36d41da3a08589185fd16
sm22100 <44148864+sm22100@users.noreply.github.com>
2020-10-24 15:52:25 -0400

    kek box

---
+++ Dead-Engines-Game/Assets/Scripts/LogicHandlers/TestSelectBox.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+
+public class TestSelectBox : MonoBehaviour
+{
+	// straight from:
+	// https://answers.unity.com/questions/1575087/how-can-i-draw-a-box-ingame.html
+
+	bool dragging = false;
+	Rect selectionBox;
+	Vector3 initialMousePosition;
+	Vector3 currentMousePosition;
+	Event e;
+
+	void OnGUI()
+	{
+		e = Event.current;
+        if (e.type == EventType.MouseDrag)
+		{
+			if (!dragging)
+			{
+				dragging = true;
+				initialMousePosition = e.mousePosition;
+			}
+		}
+        if (e.type == EventType.MouseUp)
+		{
+			dragging = false;
+		}
+        if (dragging)
+		{
+			currentMousePosition = e.mousePosition;
+			selectionBox = new Rect(initialMousePosition.x, initialMousePosition.y, currentMousePosition.x - initialMousePosition.x, currentMousePosition.y - initialMousePosition.y);
+			GUI.Box(selectionBox, GUIContent.none);
+		}
+	}
+}
