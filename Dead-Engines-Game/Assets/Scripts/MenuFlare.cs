5b020c43241741d72d1f43e7daf814afb5b5c3ec
sm22100 <44148864+sm22100@users.noreply.github.com>
2020-12-08 18:26:45 -0500

    menu pizazz

---
+++ Dead-Engines-Game/Assets/Scripts/MenuFlare.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFlare : MonoBehaviour
+{
+	public Light flash;
+	public float flashSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		flash.intensity += 1 * flashSpeed;
+		if (flash.intensity > 1.75f)
+		{
+			flash.intensity = 1.7f;
+		}
+    }
+}
