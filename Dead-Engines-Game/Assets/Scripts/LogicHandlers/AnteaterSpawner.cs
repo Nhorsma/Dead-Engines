4944bbcb30000e47175358c83552a48ddfd4fb61
Nicholas Horsma <indianablast@gmail.com>
2021-05-01 16:56:16 -0400

    I know how to fix the explosion problem, but it will take a while

---
+++ Dead-Engines-Game/Assets/Scripts/LogicHandlers/AnteaterSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnteaterSpawner : MonoBehaviour
+{
+    public HunterHandler handler;
+
+    private void Start()
+    {
+        handler = FindObjectOfType<HunterHandler>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Robot")
+        {
+            handler.SpawnAnteater();
+            handler.isDeployed = true;
+            gameObject.SetActive(false);
+        }
+    }
+
+}
